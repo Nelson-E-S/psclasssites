e31618399321cb9d7407d0d4b2ff6b0f5e6c5ccf
Student_XGA125 <Student_XGA125@macbook-air.home>
2020-07-07 12:41:42 -0400

    m1w3d1e1 edits

--- classsite/jscript/m1w3d1/headermanips.js
+++ classsite/jscript/m1w3d1/headermanips.js
@@ -3,24 +3,48 @@ const h2 = document.getElementById('big');
 const h5 = document.getElementById('normal');
 
 const getRndNum = () => {
-    return Math.floor(Math.random() * 3 + 1);
+    return Math.floor(Math.random() * 6 + 1);
+}
+const iSeeAll = () => {
+    h2.style.display = "block";
+    h5.style.display = "block";
 }
 const modifyHeader = () => {
     let rndNum = getRndNum();
     switch (rndNum){
         case 1:{
             h1.style.fontStyle = 'italic';
+            iSeeAll();
             break;
         }
         case 2:{
             h2.innerHTML = "This is <span class=\"highlight\">Awesome!</span>";
+            iSeeAll();
             break;
         }
         case 3:{
             h5.style.color = "blue";
+            iSeeAll();
+            break;
+        }
+        case 4:{
+            h1.style.fontSize = "52pt";
+            iSeeAll();
+            break;
+        }
+        case 5:{
+            h1.style.fontStyle = "normal";
+            h2.style.display = "none";
+            h5.style.display = "block";
+            break;
+        }
+        case 6: {
+            h1.style.fontStyle = "normal";
+            h2.style.display = "block";
+            h5.style.display = "none";
             break;
         }
         default:
-            alert('Issue with modifyHeader funtionc!');
+            alert('Issue with modifyHeader function!');
     }
 }
