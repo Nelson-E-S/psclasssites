857efc660b3100ed3e5e21e6c0bda418a9b76253
Student_XGA125 <Student_XGA125@macbook-air.home>
2020-07-07 11:32:27 -0400

    m1w3d1e2 added

---
+++ classsite/jscript/m1w3d1/imagemanips.js
@@ -0,0 +1,8 @@
+const img = document.getElementById('sunmoon');
+
+const changeSun = () =>{
+    img.src = "../../images/m1w3d1/sun.jpg";
+}
+const changeMoon = () =>{
+    img.src = "../../images/m1w3d1/moon.jpg";
+}
