765c08e8052bcd772b6318db772fa6cd05779797
Student_XGA125 <Student_XGA125@macbook-air.home>
2020-07-07 15:52:02 -0400

    variable on dom exmaple added

---
+++ classsite/jscript/m1w3d2/variablecheck.js
@@ -0,0 +1,16 @@
+const rnd = () => {return Math.floor(Math.random() * 10 + 1);};
+let x = rnd(),
+    y = rnd(),
+    z = rnd();
+const xElem = document.getElementById('xVal');
+const yElem = document.getElementById('yVal');
+const zElem = document.getElementById('zVal');
+
+xElem.innerHTML = x;
+
+const findY = () => {
+    yElem.innerHTML = y;
+}
+const findZ = () => {
+    zElem.innerHTML = z;
+}
