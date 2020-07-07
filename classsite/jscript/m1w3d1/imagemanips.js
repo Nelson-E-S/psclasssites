58aaaecb575a9d801e9eba0ba7e2378851cac97b
Student_XGA125 <Student_XGA125@macbook-air.home>
2020-07-07 12:26:32 -0400

    edits for m1w3d1e2

--- classsite/jscript/m1w3d1/imagemanips.js
+++ classsite/jscript/m1w3d1/imagemanips.js
@@ -1,8 +1,17 @@
 const img = document.getElementById('sunmoon');
+const btnSun = document.getElementById('sun');
+const btnMoon = document.getElementById('moon');
+const shuttle = document.getElementById('shuttle');
 
 const changeSun = () =>{
     img.src = "../../images/m1w3d1/sun.jpg";
-}
+    btnSun.style.fontSize = "12pt";
+    btnMoon.style.fontSize = "10pt";
+    shuttle.style.display = "none";
+};
 const changeMoon = () =>{
     img.src = "../../images/m1w3d1/moon.jpg";
-}
+    btnMoon.style.fontSize = "12pt";
+    btnSun.style.fontSize = "10pt";
+    shuttle.style.display = "block";
+} ;
