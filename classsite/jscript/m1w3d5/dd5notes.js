6163cfb22bd44ada1708ac17c462c3186d226160
Nelson-E-S <nelson.e.sanabria1@gmail.com>
2020-07-10 12:08:33 -0400

    edit d5 notes

--- classsite/jscript/m1w3d5/dd5notes.js
+++ classsite/jscript/m1w3d5/dd5notes.js
@@ -40,3 +40,45 @@ print("Lets go to Mars!");
 spaceship.flyTo("Mars");
 print(`Launching spaceship...`);
 print(`Your location: ${spaceship.location}`);
+
+print('');
+//const print = args => {console.log(args);};
+var robot = {
+    name: "",
+    x: 0,
+    y: 0,
+    move: function(steps, direction){
+        /*this can be done with if statements simply by breaking out each case into an if statement...I like using switches to easily cover invalid options*/
+        switch(direction.toLowerCase()){
+            case 'forward':{
+                this.x += steps;
+                break;
+            }
+            case 'backward':{
+                this.x -= steps;
+                break;
+            }
+            case 'up':{
+                this.y += steps;
+                break;
+            }
+            case 'down':{
+                this.y -= steps;
+                break;
+            }
+            default:{
+                print('Invalid direction!');
+            }
+        }
+        this.location();
+    },
+    location: function(){
+        print("### Robot coordinates ###");
+        print(`x: ${this.x} y: ${this.y}`);
+    }
+}
+var directions = ['forward','backward','up','down'];
+var rndDir = () => Math.floor(Math.random() * (directions.length - 1));
+var rndSteps = () => Math.floor(Math.random() * 10) + 1;
+for(let i = 0; i < 10; i++)
+    robot.move(rndSteps(),directions[rndDir()]);
