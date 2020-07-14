3b7fefca046bf54f418fad934cc1228f339b902b
Nelson-E-S <nelson.e.sanabria1@gmail.com>
2020-07-14 12:55:33 -0400

    added m1w4d1 .js notes

---
+++ classsite/jscript/m1w4d1/d1notes.js
@@ -0,0 +1,170 @@
+let hotelCalifornia = {
+    name: 'Hotel California',
+    location: 'California',
+    rating: 4,
+    vacancies: true,
+    tags: [
+        'pink champagne',
+        'wine',
+        'lovely',
+        'can\'t leave'
+    ],
+    rooms: [{
+            'roomNumber': 102,
+            'size': 'Queen Double',
+            'price': 55,
+            'booked': true
+        },
+        {
+            'roomNumber': 202,
+            'size': 'King Suite',
+            'price': 68,
+            'booked': false
+        },
+        {
+            'roomNumber': 404,
+            'size': 'Queen Double',
+            'price':60,
+            'booked': false
+        }
+    ]
+};
+
+class HotelRoom{
+    constructor(rn){
+        this.roomNumber = rn;
+        this.size = '';
+        this.price = 0;
+        this.booked = false;
+    };
+    getRoomNumber(){
+        return this.roomNumber;
+    };
+    getSize(){
+        return this.size;
+    };
+    getPrice(){
+        return this.price;
+    };
+    getBooked(){
+        return this.booked;
+    };
+    setSize(s){
+        this.size = s;
+    };
+    setPrice(p){
+        this.price = p;
+    };
+    setBooked(b){
+        this.booked = b;
+    };
+}
+
+class Hotel{
+    constructor (name,loc,rating,vac){
+        this.name = name; 
+        this.location = loc;
+        this.rating = rating;
+        this.vacancies = vac;
+        this.tags = [];
+        this.rooms = [];
+    };
+    getName(){
+        return this.name;
+    };
+    getLocation(){
+        return this.location;
+    };
+    getRating(){
+        return this.rating;
+    };
+    getVacancies(){
+        return this.vacancies;
+    };
+    getTags(){
+        return this.tags;
+    };
+    getRooms(){
+        return this.rooms;
+    };
+    getRoomNumbers(){
+        let roomNumbers = [];
+        this.getRooms().forEach((rm)=>{
+            roomNumbers.push(rm.roomNumber);
+        });
+        return roomNumbers;
+    }
+    setName(n){
+        this.name = n;
+    };
+    setLocation(l){
+        this.location = l;
+    };
+    setRating(r){
+        this.location = r;
+    };
+    setVacancies(v){
+        this.vacancies = v;
+    };
+    addTags(t){
+        if(t.length === 1){
+            this.tags.push(t);
+        } else if(t.length > 1){
+            for(let tag of t)
+                this.tags.push(tag);
+        }
+    };
+    removeTags(t){
+        if(this.tags.length){
+            for(let tag in this.tags){
+                if(this.tags[tag] === t){
+                    this.tags.splice(tag,1);
+                    break;
+                }
+            };
+        };
+    };
+    addRooms(rm){
+        this.rooms.push(rm);
+    };
+    removeRooms(rn){
+        if(this.rooms.length){
+            for(let room in this.rooms){
+                if(this.rooms[room].roomNumber === rn){
+                    this.rooms.splice(room,1);
+                    break;
+                }
+            }
+        }
+    }
+}
+
+const myHotel = new Hotel('Air Hotel','New York',4.5,true);
+myHotel.addTags(['fancy']);
+myHotel.addTags(['chic','comfy']);
+const room1 = new HotelRoom(102);
+room1.setSize('Queen Double');
+room1.setPrice(55);
+room1.setBooked(true);
+const room2 = new HotelRoom(202);
+room2.setSize('King Suite');
+room2.setPrice(68);
+room2.setBooked(false);
+const room3 = new HotelRoom(404);
+room3.setSize('Queen Double');
+room3.setPrice(60);
+room3.setBooked(false);
+myHotel.addRooms(room1);
+myHotel.addRooms(room2);
+myHotel.addRooms(room3);
+//console.log(room1.getRoomNumber());
+//console.log(room2);
+//console.log(room3);
+//let rms = [room1,room2,room3];
+/*rms.forEach((rms)=>{
+    console.log(rms.getRoomNumber())
+})*/
+console.log('');
+console.log(`Hotel Name: ${myHotel.getName()}\nHotel Location: ${myHotel.getLocation()}\nHotel Ratings: ${myHotel.getRating()}`);
+console.log(`Hotel Tags: ${myHotel.getTags()}`)
+console.log(`Hotel Rooms: ${myHotel.getRoomNumbers()}`)
